193760892ba3f160668d1afeaffea44e1f83e57f
Angel <86986999+DefinitelyNotAngel@users.noreply.github.com>
2026-06-03 08:33:50 -0400

    Update script.js

--- js/script.js
+++ js/script.js
@@ -124,6 +124,7 @@ function initializePlaylist() {
     addSongToPlaylist('./muzica/13 mr cellophane.mp3', 'mr cellophane');
     addSongToPlaylist('./muzica cu mc/all i care about pentru billy.mp3', 'all i care about pentru billy');
     addSongToPlaylist('./muzica cu mc/15 nowadays.mp3', 'nowadays cu mc');
+    addSongToPlaylist('./muzica cu mc/mc hot honey singur.mp3', 'mc hot honey');
     addSongToPlaylist('./muzica cu mc/ final mc.mp3', 'mcul final');
     addSongToPlaylist('./muzica/1 + 2 overture.all that jazz.mp3', 'all that jazz');
     addSongToPlaylist('./muzica cu mc/Anastacia - Love Is A Crime.mp3', 'love is a crime');
